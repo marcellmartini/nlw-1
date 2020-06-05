b1ebeafdec898b78b42bb605b61aecf9db32951d
Marcell Martini <marcellmartini@gmail.com>
2020-06-04 23:30:57 -0400

    feat: Arquivos adicionados no quarto dia da #NLW
    * Projeto Expo
    * Página Home
    * Configurando navegação
    * Página de mapas
    * Página de detalhes
    * Busca de itens na API
    * Busca de pontos na API
    * Buscando UF e cidade
    * Fix rota points da api

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -25,7 +25,7 @@ class PointsController {
   async show(request: Request, response: Response) {
     const { id } = request.params;
 
-    const point = await knex('points').where('id', id);
+    const point = await knex('points').where('id', id).first();
 
     if (!point) {
       return response.status(400).json({ message: 'Point not found.' });
